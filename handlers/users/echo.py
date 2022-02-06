2a515c71f92481d7673b7e6cbb0e650876636bd4
Vitaliy Sekach <50178320+SekachVitaliy@users.noreply.github.com>
2022-02-06 14:32:37 +0200

    add echo nadler without state

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -11,21 +11,17 @@ from utils.misc import rate_limit
 @dp.message_handler(IsPrivate(), state=Game.game)
 async def bot_echo(message: types.Message):
     """
-    Эхо хендлер, куда летят ВСЕ сообщения без состояния
-    Использовался для проверки
+    Эхо хендлер, куда летят ВСЕ сообщения c игры, если не попало в другие хендлеры
     """
     await message.answer(text="Ты в игре! Используй команду"
                               " /stop что бы выйти", reply_markup=types.ReplyKeyboardRemove())
 
 
-# @rate_limit(limit=1)
-# @dp.message_handler(IsPrivate(), state="*", content_types=types.ContentTypes.ANY)
-# async def bot_echo_all(message: types.Message, state: FSMContext):
-#     """
-#     Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
-#     Использовался для проверки
-#     """
-#     state = await state.get_state()
-#     await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-#                          f"\nСодержание сообщения:\n"
-#                          f"<code>{message}</code>")
+@rate_limit(limit=1)
+@dp.message_handler(IsPrivate())
+async def bot_echo_all(message: types.Message):
+    """
+    Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
+    Использовался для проверки
+    """
+    await message.answer(f"Выбери правильную команду).")
