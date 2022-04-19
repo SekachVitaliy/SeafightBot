c3e81a3620635e241e8dec29c165a541ce19c47e
Vitaliy Sekach <50178320+SekachVitaliy@users.noreply.github.com>
2022-04-19 13:48:07 +0300

    add command to all states in FSM

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -3,11 +3,16 @@ from aiogram import types
 from data.config import ADMINS
 from filters import IsPrivate
 from loader import db, dp
+from states import Game
 from utils.misc import rate_limit
 
 
 @rate_limit(limit=1)
 @dp.message_handler(IsPrivate(), user_id=ADMINS, text="admin")
+@dp.message_handler(IsPrivate(), user_id=ADMINS, text="admin", state=Game.not_started)
+@dp.message_handler(IsPrivate(), user_id=ADMINS, text="admin", state=Game.withdraw)
+@dp.message_handler(IsPrivate(), user_id=ADMINS, text="admin", state=Game.game)
+@dp.message_handler(IsPrivate(), user_id=ADMINS, text="admin", state=Game.pay)
 async def admin_chat(message: types.Message):
     """
     Хендлер работает по слову 'admin' в чат. Можно будет прикрипить количество людей, сыгранных игр и тд.
